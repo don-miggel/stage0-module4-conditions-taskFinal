5b4213f6a26ac5c2aee472e3169c8cce3f2123da
Mykhailo Derecha <don.miggel@yahoo.com>
2023-02-25 17:04:12 +0200

    Task 4, final modified v.2

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,9 +2,9 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-        int daysInMonth = 0;
-        if (month < 1 || month > 12 || year < 0)
-            System.out.println("invalid date");
+        int daysInMonth;
+        if (year < 0)
+            daysInMonth =0;
         else {
             daysInMonth = switch (month){
 
@@ -12,19 +12,30 @@ public class DaysInMonth {
                     yield 31;
                 }
                 case  2 -> {
-                    yield (year % 4 == 0) ? 29 : 28;
+//                    yield ((year % 4 == 0) && (year % 100 != 0)) ? 29 : 28;
+                    if (year % 4 == 0){
+                        if ((year % 400 == 0) | (year % 100 == 0 | ((year % 100) % 4 == 0)))
+                            yield 29;
+                        else
+                            yield 28;
+
+                    }
+                    else
+                        yield 28;
                 }
                 case 4,6,9,11 -> {
                     yield 30;
                 }
+
                 default -> {
                     yield 0;
                 }
 
+
             };
 
         }
-        System.out.println(daysInMonth);
+        System.out.println((daysInMonth != 0) ? daysInMonth : "invalid date");
 
     }
 }
